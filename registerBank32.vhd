e5ed6f2604cf95d54a5f617ff007b96e00c183c9
MatheusMarotzke <matheus.marotzke@gmail.com>
2017-11-17 18:39:46 -0200

    fase de testes

--- registerBank32.vhd
+++ registerBank32.vhd
@@ -7,7 +7,7 @@ entity registerBank32 is
     DadoLidoReg1        : out std_logic_vector(31 downto 0);
     DadoLidoReg2        : out std_logic_vector(31 downto 0);
     DadoEscritoReg3       : in  std_logic_vector(31 downto 0);
-    habEscritaReg : in  std_logic;
+    habEscritaReg : in  std_logic:= '0';
     EndReg1     : in  std_logic_vector(4 downto 0);
     EndReg2     : in  std_logic_vector(4 downto 0);
     EndReg3 : in  std_logic_vector(4 downto 0);
